0e0ade8eee99993479d0aaa760f6e6ebee366352
Kallestarsz <skhunters14@gmail.com>
2023-02-09 18:49:14 +0200

    Add function that determines a winner

--- script.js
+++ script.js
@@ -10,10 +10,25 @@ const gameBoard = (() => {
         }
     } 
 
+    function getWinner() {
+        for (let i = 0; i < 3; i++) {
+            if (board[0][i] === board[1][i] & board[0][i] === board[2][i]) {
+                return board[0][i];
+            } else if (board[i][0] === board[i][1] & board[i][0] === board[i][2]) {
+                return board[i][0];
+            }
+        }
+        if (board[0][0] === board[1][1] & board[0][0] === board[2][2]){
+            return board[0][0];
+        } else if (board[0][2] === board[1][1] & board[0][2] === board[2][0]) {
+            return board[0][2];
+        }
+    }
+    
     function playRound(playerX, playerO, round){
         for (let i = 0; i < boardDOM.length; i++) {
             boardDOM[i].addEventListener("click", (event) => {
-                    if (board[i] = " ") { 
+                    if (board[i] === " ") { 
                         if(playerX.turn){
                             board[i] = playerX.sign;
                             playerX.turn = false;
@@ -26,6 +41,7 @@ const gameBoard = (() => {
                         }
                     }
                 display();
+                console.log(getWinner());
             });
         }
     }
