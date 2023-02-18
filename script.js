1151590392c4f7ceef258c82ab068916b531988c
Kallestarsz <skhunters14@gmail.com>
2023-02-18 13:16:32 +0200

    Fix getWinner function

--- script.js
+++ script.js
@@ -11,18 +11,32 @@ const gameBoard = (() => {
     } 
 
     function getWinner() {
-        for (let i = 0; i < 3; i++) {
-            if (board[0][i] === board[1][i] & board[0][i] === board[2][i]) {
-                return board[0][i];
-            } else if (board[i][0] === board[i][1] & board[i][0] === board[i][2]) {
-                return board[i][0];
-            }
-        }
-        if (board[0][0] === board[1][1] & board[0][0] === board[2][2]){
-            return board[0][0];
-        } else if (board[0][2] === board[1][1] & board[0][2] === board[2][0]) {
-            return board[0][2];
-        }
+         // Check rows
+         for (let i = 0; i < 3; i++) {
+           const rowStartIndex = i * 3;
+           if (board[rowStartIndex] === board[rowStartIndex + 1] && board[rowStartIndex + 1] === board[rowStartIndex + 2] && board[rowStartIndex] !== '') {
+             return board[rowStartIndex];
+           }
+         }
+        
+         // Check columns
+         for (let j = 0; j < 3; j++) {
+           if (board[j] === board[j + 3] && board[j + 3] === board[j + 6] && board[j] !== '') {
+             return board[j];
+           }
+         }
+        
+         // Check diagonals
+         if (board[0] === board[4] && board[4] === board[8] && board[0] !== '') {
+           return board[0];
+         }
+        
+         if (board[2] === board[4] && board[4] === board[6] && board[2] !== '') {
+           return board[2];
+         }
+        
+         // If no winner is found, return null
+         return null;
     }
     
     function playRound(playerX, playerO, round){
