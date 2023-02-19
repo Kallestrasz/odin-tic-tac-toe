34ac32d210eba85a9615c1930cbd1a3bbe7b5ddf
Kallestarsz <skhunters14@gmail.com>
2023-02-19 13:09:42 +0200

    Add a bot that decides it's next move randomly

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const gameBoard = (() => {
     let boardDOM = Array.from(document.querySelectorAll(".field"));
                 
     function display(){
+        //makes changes on the board visible in html
         for (let i = 0; i < board.length; i++) {
             boardDOM[i].innerText = board[i]; 
         }
@@ -39,19 +40,30 @@ const gameBoard = (() => {
          return null;
     }
     
-    function playRound(playerX, playerO, round){
+    function playRound(playerX, playerO){
         for (let i = 0; i < boardDOM.length; i++) {
             boardDOM[i].addEventListener("click", (event) => {
                     if (board[i] === " ") { 
-                        if(playerX.turn){
-                            board[i] = playerX.sign;
-                            playerX.turn = false;
-                            playerO.turn = true;
+                       if(playerX.turn){
+                         board[i] = playerX.sign;
+                         let swap = playerX.turn
+                         if(playerO.bot != 0) board[playerO.botMove()] = playerO.sign;
+                         else{
+                          playerX.turn = playerO.turn;
+                          playerO.turn = swap;
+                         }
                         }
                         else {
+                          if(playerX.bot != 0) {
+                          board[playerX.botMove()] = playerX.sign;
+                          board[i] = playerO.sign;
+                          }
+                          else {
                             board[i] = playerO.sign;
-                            playerO.turn = false;
-                            playerX.turn = true;
+                            let swap = playerX.turn
+                            playerX.turn = playerO.turn;
+                            playerO.turn = swap;
+                          }
                         }
                     }
                 display();
@@ -60,18 +72,37 @@ const gameBoard = (() => {
         }
     }
 
-    return{getWinner, board, playRound}
+    return{playRound, board}
     
 })();
 
 const player = (sign, name, bot, turn) => {
-    return{sign, name, bot, turn}
+  // bot = 0 --> not a bot
+  // bot = 1 --> random input bot
+  // but = 2 --> minimax bot
+  function botMove(){
+   if(bot === 1){
+      let available = [];
+      for (let i = 0; i < 9; i++) {
+        if(gameBoard.board[i] == " ") available.push(i);
+      }
+      randomMove = available[[Math.floor(Math.random()*available.length)]];
+      return randomMove;
+    }
+    // else if(bot === 2){
+
+    // }
+    // else return null;
+  } 
+  return{sign, name, bot, turn, botMove}
 }
+
+
 
 const game = (() => {
 
-    const playerX = player("X","amongus", false, true);
-    const playerO = player("O","sugoma", false, false);
+    const playerX = player("X","amongus", 0, true);
+    const playerO = player("O","sugoma", 1, false);
 
     function gaming(){
         gameBoard.playRound(playerX,playerO,0);
